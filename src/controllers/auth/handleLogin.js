52990d8b1061b693a7c752443c443e2a75f8accc
Pamela <pamela_bhopal@hotmail.co.uk>
2021-06-14 22:31:08 +0100

    handle login

--- src/controllers/auth/handleLogin.js
+++ src/controllers/auth/handleLogin.js
@@ -1,28 +1,40 @@
 const { User } = require("../../models");
 
-const handleSignUp = async (req, res) => {
+const handleLogin = async (req, res) => {
   try {
     const username = req.body.username;
     const password = req.body.password;
 
-    const user = await User.create({
-      username: username,
-      password: password,
+    const user = await User.findOne({
+      where: {
+        username: username,
+      },
     });
 
     if (!user) {
-      console.log("Failed to create user");
-      return res.status(500).json({ error: "Failed to signup" });
+      console.log("User does not exists");
+      return res.status(401).json({ error: "Failed to login" });
     }
 
-    return res.status(200).json({ message: "success" });
+    if (user.password !== password) {
+      console.log("Incorrect password");
+      return res.status(401).json({ error: "Failed to login" });
+    }
+
+    req.session.save(() => {
+      req.session.isLoggedIn = true;
+      req.session.username = user.username;
+      req.session.userId = user.id;
+
+      return res.status(200).json({ message: "success" });
+    });
   } catch (error) {
     console.log(error.message);
-    return res.status(500).json({ error: "Failed to signup" });
+    return res.status(500).json({ error: "Failed to login" });
   }
 };
 
-module.exports = handleSignUp;
+module.exports = handleLogin;
 
 // get data from post body
 // get the user
