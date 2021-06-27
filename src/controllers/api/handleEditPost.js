9972c54b2f6324d965e4ead42bb03506d3927c02
Suraj Verma <surajverma@rentalcars.com>
2021-06-27 15:12:50 +0100

    delete post controllers

--- src/controllers/api/handleEditPost.js
+++ src/controllers/api/handleEditPost.js
@@ -1,5 +1,17 @@
-const handleEditPost = (req, res) => {
-  res.json({});
+const { Post } = require("../../models");
+
+const handleEditPost = async (req, res) => {
+  const { id } = req.params;
+  const { title, body } = req.body;
+
+  const updatedPost = {
+    title,
+    body,
+  };
+
+  await Post.update(updatedPost, { where: { id } });
+
+  res.status(200).json({ message: "success" });
 };
 
 module.exports = handleEditPost;
